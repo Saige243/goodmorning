7dd40be319386f9c36f822671176d278e78324fb
Saige Cross <saigecrossdev@gmail.com>
2025-08-10 14:35:26 -0500

    first commit

---
+++ good_morning.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import webbrowser
+
+gmail_url = "https://mail.google.com/"
+linkedin_url = "https://www.linkedin.com/jobs/search-results/?keywords=remote%20NOT%20senior&origin=JOB_COLLECTION_PAGE_SEARCH_BUTTON"
+job_spreadsheet = "https://docs.google.com/spreadsheets/d/1BTbenZ_DkEgKZ8tEVbYqy6KrDyoCPJ5YL999bxGVfYE/edit?gid=0#gid=0"
+
+webbrowser.open(gmail_url)
+webbrowser.open(linkedin_url)
+webbrowser.open(job_spreadsheet)
