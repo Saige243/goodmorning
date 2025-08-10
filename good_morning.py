80db035e06c465514e4ed53f601cc4a9977f1dfd
Saige Cross <saigecrossdev@gmail.com>
2025-08-10 18:28:00 -0500

    add lifeboard to script

--- good_morning.py
+++ good_morning.py
@@ -8,13 +8,16 @@ linkedin_url = "https://www.linkedin.com/jobs/search-results/?keywords=remote%20
 job_spreadsheet = "https://docs.google.com/spreadsheets/d/1BTbenZ_DkEgKZ8tEVbYqy6KrDyoCPJ5YL999bxGVfYE/edit?gid=0#gid=0"
 calendar = "https://calendar.google.com/calendar/u/0/r"
 votd = "https://www.bible.com/verse-of-the-day"
+life_board = "https://www.figma.com/board/D1fZyDrVqo6AsG3M9eKSBJ/Life-Board?node-id=0-1&p=f&t=Osk2iEVQ7CkKyXv4-0"
 
 webbrowser.open(gmail_url)
 webbrowser.open(linkedin_url)
 webbrowser.open(job_spreadsheet)
 webbrowser.open(calendar)
 webbrowser.open(votd)
+webbrowser.open(life_board)
 
 # Open Apps
 subprocess.run(["open", "-a", "Flow"])
 subprocess.run(["open", "-a", "Todoist"])
+subprocess.run(["open", "-a", "ChatGPT"])
