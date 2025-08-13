23f1115197d9b6748a011fd32bdabda5c2eb6aac
Saige Cross <saigecrossdev@gmail.com>
2025-08-13 07:52:07 -0500

    add more links, replace others

--- good_morning.py
+++ good_morning.py
@@ -8,7 +8,9 @@ linkedin_url = "https://www.linkedin.com/jobs/search-results/?keywords=remote%20
 job_spreadsheet = "https://docs.google.com/spreadsheets/d/1BTbenZ_DkEgKZ8tEVbYqy6KrDyoCPJ5YL999bxGVfYE/edit?gid=0#gid=0"
 calendar = "https://calendar.google.com/calendar/u/0/r"
 votd = "https://www.bible.com/verse-of-the-day"
-life_board = "https://www.figma.com/board/D1fZyDrVqo6AsG3M9eKSBJ/Life-Board?node-id=0-1&p=f&t=Osk2iEVQ7CkKyXv4-0"
+life_board = "https://www.figma.com/board/9Y5tH4bglD4AYMkcQO1u8r/Code-Life-Board?t=JSm0qdLuquRbD4bR-0"
+exercism = "https://exercism.org/tracks/typescript"
+dan = "https://overreacted.io/"
 
 webbrowser.open(gmail_url)
 webbrowser.open(linkedin_url)
@@ -16,6 +18,8 @@ webbrowser.open(job_spreadsheet)
 webbrowser.open(calendar)
 webbrowser.open(votd)
 webbrowser.open(life_board)
+webbrowser.open(exercism)
+webbrowser.open(dan)
 
 # Open Apps
 subprocess.run(["open", "-a", "Flow"])
